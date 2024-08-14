f4382941ca5a8ceafa5685625c0ff4037d584a98
Jakub Mesticky <mesticky.jakub7@gmail.com>
2024-08-14 16:57:10 +0200

    adding configuration and modules

---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+import { TableMenuComponent } from './table-menu/table-menu.component';
+import { TableRowComponent } from './table-row/table-row.component';
+
+export { TableMenuComponent, TableRowComponent };
